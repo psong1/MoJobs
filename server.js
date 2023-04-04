649282482251122a459c21481287d7fe105fb72b
pguerra98 <119546662+pguerra98@users.noreply.github.com>
2023-04-03 20:54:35 -0400

    added in info to config js & server js

---
+++ server.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+const express = require('express');
+
+const session = require('express-session');
+
+const expbars = require('express-handlebars');
+
+const routes = require('');
+
+const helpers = require('');
+
+const sequelize = require('./config/config');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const hbs = expbars.create({helpers});
+
+const ses = {
+
+    secret: 'Job Board',
+
+    cookie: {},
+
+    resave: false,
+
+    saveUninitialzed: true,
+
+    store: new SequelizeStore({db:sequelize})
+};
+
+app.use(session(ses));
+
+app.engine('handlebars', hbs.engine);
+
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+
+    console.log(`App is listening on port ${PORT}.`);
+
+    sequelize.sync({force:false});
+
+})
